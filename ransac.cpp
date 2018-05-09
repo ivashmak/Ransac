7eb47c0705ae741abcf2330cd83b4e693fd8cf9d
ivashmak <ivashmak@fel.cvut.cz>
2018-05-09 17:05:35 +0200

    draw lines

--- ransac.cpp
+++ ransac.cpp
@@ -31,6 +31,9 @@ void line_func (float x1, float x2, float y1, float y2) {
 }
 
 
+void draw_function (float k, float b, float max_dimen, cv::Scalar color, cv::Mat img);
+bool fit_point (float x, float y, float k, float b1, float b2);
+
 int main () {
 	srand (time(NULL));
 
@@ -64,26 +67,32 @@ int main () {
 		
 		int quantity_in_polygon = 0;
 
-		for (int k = 0; k < keypoints.size(); k++) {
-			if (keypoints[k].pt.x) {
+		float k = (x2-x1)/(y2-y1);
+		float b = (y1*x1- y1*x2)/(y2-y1) + x1;
 
+		std::vector<cv::Point> fit_points;
+		
+		for (int k = 0; k < keypoints.size(); k++) {
+			if (fit_point(keypoints[k].pt.x, keypoints[k].pt.y, k, b-threshold, b+threshold)) {
+				fit_points.push_back(cv::Point(keypoints[k].pt.y, keypoints[k].pt.x));
 			}
 		}
 
-		float k = (x2-x1)/(y2-y1);
-		float b = (x2*y1- x1*y2)/(x2-x1);
-
-		float corner_x1 = 0;
-		float corner_y1 = (height-b)/k;
-		
-		float corner_x2 = width;
-		float corner_y2 = 0;
 		
 		img = cv::imread("data/image1.jpg");
-		cv::line (img, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(110, 220, 0),  2, 8 );
-		cv::line (img, cv::Point(corner_x1, corner_y1), cv::Point(corner_x2, corner_y2), cv::Scalar(234, 33, 54),  2, 8 );
+		draw_function (k, b, std::max(width, height), cv::Scalar(255,0,0), img);
+		draw_function (k, b+threshold, std::max(width, height), cv::Scalar(0,0,255), img);
+		draw_function (k, b-threshold, std::max(width, height), cv::Scalar(0,0,255), img);		
+		cv::line (img, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(110, 220, 0), 2); //thickness
 		
+
+		// cv::Mat img_keypoints;
+		// drawKeypoints (img, line_points, img_keypoints, cv::Scalar::all(-1), cv::DrawMatchesFlags::DEFAULT);
+  		
+		// imshow("Image",img_keypoints);
 		imshow("Image",img);
+  		
+
   		cv::waitKey( 0 );
 
 		iter++;	
@@ -94,4 +103,20 @@ int main () {
 	// }	
 
 	return 0;
+}
+
+void draw_function (float k, float b, float max_dimen, cv::Scalar color, cv::Mat img) {
+	float corner_y1 = max_dimen;
+	float corner_x1 = k*max_dimen+b;
+	
+	float corner_y2 = -max_dimen;
+	float corner_x2 = k*(-max_dimen)+b;
+	cv::line (img, cv::Point(corner_x1, corner_y1), cv::Point(corner_x2, corner_y2), color,  2, 8 );
+}
+
+bool fit_point (float x, float y, float k, float b1, float b2) {
+	// if () {
+
+	// }
+	return true;
 }
